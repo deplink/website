2d753b984495ac12f688296f4accf1470410a506
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-12-05 21:53:17 +0100

    Print deploy script output

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Print outputs and exit on first failure.
+set -xe
+
+# Register Travis CI key
 eval "$(ssh-agent -s)"
 ssh-add - <<< "${$TRAVIS_KEY}"
 
+# Upload "build" directory to server
 scp -r build/* travis@deplink.org:~/
