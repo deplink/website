c0cce0c977c63bfba0e1d2be1d22456d067dde40
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2018-12-09 14:26:35 +0100

    Swipe support for navbar

--- template/libraries/.export.js
+++ template/libraries/.export.js
@@ -2,5 +2,6 @@ module.exports = [
     'jquery-3.2.1.min.js',
 	'icomoon/style.css',
     'icomoon/fonts/*',
-    'fuse.min.js'
+    'fuse-3.0.4.min.js',
+    'hammer-2.0.8.min.js'
 ];
--- template/scripts/navbar.js
+++ template/scripts/navbar.js
@@ -1,8 +1,20 @@
 module.exports = function () {
 
+    function showMenu() {
+        $('.navbar-mobile').addClass('-visible');
+    }
+
+    function hideMenu() {
+        $('.navbar-mobile').removeClass('-visible');
+    }
+
     function toggleMenu() {
         $('.navbar-mobile').toggleClass('-visible');
     }
+
+    var hammertime = new Hammer(document);
+    hammertime.on('swipeleft', hideMenu);
+    hammertime.on('swiperight', showMenu);
 
     $('.navbar-mobile-toggle').click(toggleMenu);
 
